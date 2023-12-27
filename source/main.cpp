53129c8f8b3a87f0233d9a477dddbe8ae2fac536
Ethan Chien <liangtie.qian@gmail.com>
2023-12-28 00:11:45 +0800

    Add style

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyleSheet(([](){
+        QFile file(":/qdarkstyle/light/lightstyle.css")        ;
+                        QTextStream stream (&file);\
+                            if (file.open(QIODevice::ReadOnly))
+        return stream.readAll();
+                            return QString{};
+                     })());
     MainWindow w;
     w.show();
     return a.exec();
