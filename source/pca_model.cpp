5bc4af52826367a0cf336e3e54e5f79d44a7c5be
Ethan Chien <liangtie.qian@gmail.com>
2023-12-28 01:12:35 +0800

    link libs

--- source/pca_model.cpp
+++ source/pca_model.cpp
@@ -1,11 +1,22 @@
 #include "pca_model.h"
 
-PcaModel::PcaModel()
-{
+static const auto UN_SELECTED = "--";
 
+PcaModel::PcaModel(QString const& filePath) {}
+
+PcaModel::~PcaModel() = default;
+
+auto PcaModel::getProvinces() const -> QStringList
+{
+    return _pcb.keys();
 }
 
-PcaModel::~PcaModel()
+auto PcaModel::getCities(QString province) const -> QStringList
 {
+    return _pcb[province].keys();
+}
 
+auto PcaModel::getAreas(QString province, QString city) -> QStringList
+{
+    return _pcb[province][city];
 }
--- source/pca_model.h
+++ source/pca_model.h
@@ -1,15 +1,23 @@
 #ifndef PCA_MODEL_H
 #define PCA_MODEL_H
 
+#include <QMap>
+#include <QString>
 
 class PcaModel
 {
-public:
-    PcaModel();
+  public:
+    PcaModel(QString const& filePath);
     ~PcaModel();
 
-private:
+    auto getProvinces() const -> QStringList;
 
+    auto getCities(QString province) const -> QStringList;
+
+    auto getAreas(QString province, QString city) -> QStringList;
+
+  private:
+    QMap<QString, QMap<QString, QStringList>> _pcb;
 };
 
 #endif
